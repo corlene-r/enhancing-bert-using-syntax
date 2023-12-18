c74847c1f2cd4ab3b617ac87a7839d8bb0aa4392
= <=>
2023-12-18 15:26:22 -0600

    changed visualize.py so that it new creates grouped graphs

--- visualize.py
+++ visualize.py
@@ -32,32 +32,37 @@ import pandas as pd
 
 pd.set_option("display.max.columns", None)
 
-p_df = pd.read_csv("./evals.csv")
+df = pd.read_csv("./evals.csv")
 
-dev = p_df[p_df["mode"] == "dev"]
+#print(df.groupby(["mode", "model_type", "adjacency_threshhold", "tree_threshhold"])["macro_f1"].mean())
 
-dev_modified_goemotions = dev[dev["model_type"] != "goemotions"]
+df = df[
+    (df["model_type"] != "goemotions_syntax_interleaved")
+    | ((df["tree_threshhold"] == 200) & (df["adjacency_threshhold"] == 12))
+]
+
+# Plot the plots
+
+dev = df[df["mode"] == "dev"]
+
+dev_interleaved_goemotions = dev[dev["model_type"] == "goemotions_syntax_interleaved"]
+dev_modified_goemotions = dev[dev["model_type"] == "goemotions_modified"]
 dev_goemotions = dev[dev["model_type"] == "goemotions"]
 
-print(dev.groupby("model_type")["accuracy"].mean())
+def show_plot(df: pd.DataFrame, title, color):
+    df.set_index('epoch', inplace=True)
+    df.groupby('seed')['macro_f1'].plot(legend=False)
+    plt.title(title)
+    plt.ylabel("Macro F1")
+    plt.xlabel("Epoch")
+    plt.ylim(0.3, 0.55)
+    plt.xlim(1, 10)
+    #plt.plot(df["epoch"], df["macro_f1"], color)
+    plt.show()
 
-def show_plot(df: pd.DataFrame, title, axs, axidx):
+#num_plots = 8
+#fig, axs = plt.subplots(4, 2)
 
-    groups = df.groupby("run_id") 
-    for name, group in groups:
-        axs[axidx].plot(x=group["epoch"], y=group["macro_f1"])
-        axs[axidx].ylabel("Macro F1")
-        axs[axidx].set_title(title)
-        axs[axidx].xlabel("Epoch")
-        axs[axidx].ylim(0.3, 0.55)
-        axs[axidx].xlim(1, 10)
-        axidx += 1
-
-
-num_plots = 8
-fig, axs = plt.subplots(4, 2)
-
-show_plot(dev_goemotions, "GoEmotions", axs, 0)
-show_plot(dev_modified_goemotions, "Modified GoEmotions", axs, 4)
-
-plt.show()
+show_plot(dev_goemotions, "GoEmotions", "blue")
+show_plot(dev_modified_goemotions, "Modified GoEmotions", "red")
+show_plot(dev_interleaved_goemotions, "Interleaved GoEmotions", "green")
